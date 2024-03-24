433fd1fc3310774b8b5fc855f94e417599dd07d5
anullator <danyelle.joy@gmail.com>
2024-03-23 23:53:03 -0500

    add script tags and submit button functionality

---
+++ assets/js/form.js
@@ -0,0 +1,33 @@
+// assign input content to variables
+const username = document.getElementById("username");
+const title = document.getElementById("blog-title");
+const content = document.getElementById("blog-content"); 
+const submitBtn = document.querySelector('input[type="submit"]');
+
+// save blog post content to local storage
+function submitHandler() {
+
+    // create blog post object
+    const blogPost = createPostObj();
+
+    // update local blog list
+    updateStorage(blogPost);
+} 
+
+// CREATE BLOG POST
+function createPostObj () {
+    return {
+        username: username.value.trim(),
+        title: title.value.trim(),
+        content: content.value.trim(),
+    }
+}
+
+// UPDATE LOCAL STORAGE
+function updateStorage (blogPost) {
+    const stringBlog = JSON.stringify(blogPost);
+    localStorage.setItem("blogPost", stringBlog);
+}
+
+// add event listener to submit button
+submitBtn.addEventListener("click", submitHandler);
